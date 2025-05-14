a82087a147160d2e61727fc9de8e499f4e6923e8
Radostin Petrov <radostin.petrov99@gmail.com>
2025-05-14 22:51:58 +0300

    attempt at fixing github pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/radostinpetrov.github.io' : '',
+  basePath: process.env.GITHUB_ACTIONS ? '/radostinpetrov.github.io' : '',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
+  assetPrefix: process.env.GITHUB_ACTIONS ? '/radostinpetrov.github.io' : '',
 };
 
 module.exports = nextConfig;
